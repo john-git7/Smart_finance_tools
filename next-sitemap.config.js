46bdcd850a0f832d2bc0d56b9b2ccba0553e51e1
john-git7 <gideonebenezer467@gmail.com>
2025-08-07 13:06:14 +0530

    Disable auto robots.txt and add manual version

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://smartfinancetools.vercel.app', // or your custom domain
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
 };
