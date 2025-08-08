b348f83006a78da6dfa2dc28e4cbe8902d1d2518
john-git7 <gideonebenezer467@gmail.com>
2025-08-08 14:40:06 +0530

    :)

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,6 +47,19 @@ export const metadata = {
 
 
 export default function RootLayout({ children }) {
+  const setInitialTheme = `
+    (function () {
+      try {
+        const theme = localStorage.getItem('theme');
+        if (theme === 'dark') {
+          document.documentElement.classList.add('dark');
+        } else {
+          document.documentElement.classList.remove('dark');
+        }
+      } catch (_) {}
+    })();
+  `;
+
   return (
     <html lang="en">
       <head>
